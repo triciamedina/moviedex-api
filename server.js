9816b7e4fb95d101ec74096fec2c42b24231dd4f
triciamedina <tricia.medina@gmail.com>
2019-09-29 16:58:48 -0700

    Prepare for production

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const MOVIEDEX = require('./moviedex.json');
 
 const app = express();
 
-app.use(morgan('common'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
@@ -15,8 +17,6 @@ app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN;
     const authToken = req.get('Authorization');
     
-    console.log('validate bearer token middleware');
-
     if( !authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({ error: 'Unauthorized request' });
     }
@@ -49,10 +49,19 @@ function handleGetMovies(req, res) {
     res.json(results);
 }
 
-app.get('/movie', handleGetMovies)
+app.get('/movie', handleGetMovies);
 
-const PORT = 8000;
+app.use((error, req, res, next) => {
+    let response;
+    if (process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error' } }
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response);
+})
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`);
 });
